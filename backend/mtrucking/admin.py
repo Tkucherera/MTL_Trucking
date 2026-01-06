4b25e587474b2f8472f90cb9f0bcdbcf70065939
Tinashe Kucherera <tkucherera86@gmail.com>
2026-01-06 15:13:34 -0500

    dashboard

--- backend/mtrucking/admin.py
+++ backend/mtrucking/admin.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Driver, Truck, Trip, RoutePoint, Document, Payroll, TripUpdate
 
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(BaseUserAdmin, ModelAdmin):
     list_display = ['username', 'email', 'role', 'is_active']
     list_filter = ['role', 'is_active']
     fieldsets = BaseUserAdmin.fieldsets + (
@@ -12,19 +13,19 @@ class UserAdmin(BaseUserAdmin):
     )
 
 @admin.register(Driver)
-class DriverAdmin(admin.ModelAdmin):
+class DriverAdmin(ModelAdmin):
     list_display = ['user', 'license_number', 'status', 'current_truck', 'total_miles']
     list_filter = ['status']
     search_fields = ['license_number', 'user__username', 'user__email']
 
 @admin.register(Truck)
-class TruckAdmin(admin.ModelAdmin):
+class TruckAdmin(ModelAdmin):
     list_display = ['truck_number', 'make', 'model', 'year', 'status', 'mileage']
     list_filter = ['status', 'make']
     search_fields = ['truck_number', 'vin', 'license_plate']
 
 @admin.register(Trip)
-class TripAdmin(admin.ModelAdmin):
+class TripAdmin(ModelAdmin):
     list_display = ['trip_number', 'driver', 'truck', 'status', 'scheduled_pickup', 'scheduled_delivery']
     list_filter = ['status', 'broker']
     search_fields = ['trip_number', 'load_number', 'broker']
